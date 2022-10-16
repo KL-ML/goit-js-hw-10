b55b3e56a3d6ab2434ad2999f274a71d268d6bc1
Natalya Karpova <knataly0513@gmail.com>
2022-10-16 15:54:05 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,13 +1,15 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const BASE_URL = `https://restcountries.com/v2/name/`;
 
 function fetchCountries(name) {
 const url = `${BASE_URL}${name}?fields=name,capital,population,flags,languages`;
   return fetch(url).then(
     (response) => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-          return response.json();
+        if (!response.ok) {
+            Notify.failure("Oops, there is no country with that name");
+        }
+        return response.json();
     }
   );
 }
